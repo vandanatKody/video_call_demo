eb84df0d6bd1cbf427da4a88e680e4fd79f5ad44
vandanatKody <vandan.raval@kodytechnolab.com>
2023-04-24 16:43:03 +0530

    changes

---
+++ lib/login_service.dart
@@ -0,0 +1,58 @@
+// Package imports:
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:video_call_demo/constant.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
+
+/// local virtual login
+Future<void> login({
+  required String userID,
+  required String userName,
+}) async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setString(cacheUserIDKey, userID);
+
+  currentUser.id = userID;
+  currentUser.name = 'user_$userID';
+}
+
+/// local virtual logout
+Future<void> logout() async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.remove(cacheUserIDKey);
+}
+
+/// on user login
+void onUserLogin() {
+  /// 4/5. initialized ZegoUIKitPrebuiltCallInvitationService when account is logged in or re-logged in
+  ZegoUIKitPrebuiltCallInvitationService().init(
+    appID: 1234 /*input your AppID*/,
+    appSign: 'yourAppSign' /*input your AppSign*/,
+    userID: currentUser.id,
+    userName: currentUser.name,
+    notifyWhenAppRunningInBackgroundOrQuit: false,
+    plugins: [ZegoUIKitSignalingPlugin()],
+    requireConfig: (ZegoCallInvitationData data) {
+      final config = (data.invitees.length > 1)
+          ? ZegoCallType.videoCall == data.type
+          ? ZegoUIKitPrebuiltCallConfig.groupVideoCall()
+          : ZegoUIKitPrebuiltCallConfig.groupVoiceCall()
+          : ZegoCallType.videoCall == data.type
+          ? ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
+          : ZegoUIKitPrebuiltCallConfig.oneOnOneVoiceCall();
+
+      /// support minimizing, show minimizing button
+      config.topMenuBarConfig.isVisible = true;
+      config.topMenuBarConfig.buttons
+          .insert(0, ZegoMenuBarButtonName.minimizingButton);
+
+      return config;
+    },
+  );
+}
+
+/// on user logout
+void onUserLogout() {
+  /// 5/5. de-initialization ZegoUIKitPrebuiltCallInvitationService when account is logged out
+  ZegoUIKitPrebuiltCallInvitationService().uninit();
+}
